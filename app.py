3e8f085579d758313a82467be5f48e1eba920e16
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-04-12 15:06:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from daily_signals import daily_signals_app
 from indicies import indicies_app
 from positions import positions_app
 from single_names import single_names_app
@@ -17,7 +18,9 @@ def main():
         ],
     )
 
-    if app_selection == "Indices":
+    if app_selection == "Daily Signals":
+        daily_signals_app()
+    elif app_selection == "Indices":
         indicies_app()
     elif app_selection == "Positions":
         positions_app()
