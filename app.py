dfd83ff0a91051baa8f5dd31e10a056c307b9455
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-04-12 15:57:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,28 @@ from pages import daily_signals, indicies, positions, single_names
 
 st.set_page_config(page_title="Multi-Page Dashboard", page_icon=":chart_with_upwards_trend:")
 
-def main():
-    st.sidebar.title("Navigation")
-    app_selection = st.sidebar.selectbox(
-        "Choose a page:",
-        options=[
-            "Daily Signals",
-            "Indices",
-            "Positions",
-            "Single Names",
-        ],
-    )
+def daily_signals_page():
+    st.sidebar.markdown("# Daily Signals")
+    daily_signals.daily_signals_app()
 
-    if app_selection == "Daily Signals":
-        daily_signals
-    elif app_selection == "Indices":
-        indicies
-    elif app_selection == "Positions":
-        positions
-    elif app_selection == "Single Names":
-        single_names
+def indicies_page():
+    st.sidebar.markdown("# Indices")
+    indicies.indicies_app()
 
-if __name__ == "__main__":
-    main()
+def positions_page():
+    st.sidebar.markdown("# Positions")
+    positions.positions_app()
+
+def single_names_page():
+    st.sidebar.markdown("# Single Names")
+    single_names.single_names_app()
+
+page_names_to_funcs = {
+    "Daily Signals": daily_signals_page,
+    "Indices": indicies_page,
+    "Positions": positions_page,
+    "Single Names": single_names_page,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
