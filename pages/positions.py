a867cb5110865cda0b4bab9d88a97925207afe1d
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-04-17 08:31:17 -0400

    Update positions.py

--- pages/positions.py
+++ pages/positions.py
@@ -513,7 +513,8 @@ def seasonals_chart(tick):
 	)
 	st.plotly_chart(fig)
 
-positions=['OZK','JPM','PAAS','CL=F','BWA','GBPUSD=X','IWM','GOOG','BLDR','TLT']
+positions=['OZK','JPM','PAAS','CL=F','BWA','GBPUSD=X','IWM','GOOG','BLDR','TLT','NFLX','LMT','HD']
+positions.sort()
 for stock in positions:
 	seasonals_chart(stock)
 
