4866d6be9ef900099055016c4437d079197afc0b
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-05-05 13:54:34 -0400

    Update watchlist.py

--- pages/watchlist.py
+++ pages/watchlist.py
@@ -527,7 +527,7 @@ def seasonals_chart(tick):
 	    create_annotation(
 		1.02,
 		1.10,
-		f"Concordance 1,5,10,21: {sign_agreement_1d}, {sign_agreement_5d}, {sign_agreement_10d}, {sign_agreement_21d}",
+		f"5d and 21d Concordance: {sign_agreement_5d}, {sign_agreement_21d}",
 		'white'
 	    )
 	)
