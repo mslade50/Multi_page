ad51611d58d02cd1482a5719493818b6974b503a
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-05-05 13:39:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pages import daily_signals, indicies, positions, single_names, currency_pairs, commodities
+from pages import daily_signals, indicies, positions, single_names, currency_pairs, commodities, watchlist
 
 st.set_page_config(page_title="Multi-Page Dashboard", page_icon=":chart_with_upwards_trend:")
 
@@ -27,9 +27,9 @@ def commodities_page():
     st.sidebar.markdown("# Commodities")
     commodities.commodities_app()
 
-def focus_list_page():
-    st.sidebar.markdown("# Focus List")
-    focus_list.focus_list_app()
+def watchlist_page():
+    st.sidebar.markdown("# Watchlist")
+    watchlist.watchlist_app()
 
 page_names_to_funcs = {
     "Daily Signals": daily_signals_page,
@@ -38,8 +38,8 @@ page_names_to_funcs = {
     "Single Names": single_names_page,
     "Currency Pairs": currency_pairs_page,
     "Commodities": commodities_page,
+    "Watchlist": watchlist_page,
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
-
