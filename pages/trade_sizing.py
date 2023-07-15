3dd7e83781b8d560f48af7eccc363779b6ca0df8
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-07-15 10:52:34 -0400

    Update trade_sizing.py

--- pages/trade_sizing.py
+++ pages/trade_sizing.py
@@ -1,65 +1,55 @@
 import streamlit as st
 import yfinance as yf
-from ta.volatility import AverageTrueRange
+import ta
 
-# Function to calculate number of shares to buy
-# Function to calculate number of shares to buy
-# Function to calculate number of shares to buy
-def calculate_trade_size(account_size, risk_allocation, entry_level, stop_level, base_currency, target_currency):
+def calculate_trade_size(account_size, risk_allocation, entry_level, stop_level, ticker, asset_class):
     risk_per_trade = account_size * risk_allocation / 100  # Converting percentage risk allocation to absolute value
     risk_per_unit = abs(entry_level - stop_level)
 
-    # Convert risk_per_trade to target currency if necessary
-    if target_currency != base_currency:
-        forex_ticker = f"{base_currency}{target_currency}=X"
-        forex_data = yf.download(forex_ticker, period="1d")
-        
-        if forex_data.empty or pd.isnull(forex_data['Close'].iloc[0]):
-            st.error(f"Could not get exchange rate for {forex_ticker}. Please check the ticker and try again.")
-            return
-        else:
-            exchange_rate = forex_data['Close'].iloc[0]
-            risk_per_trade = risk_per_trade / exchange_rate
+    if asset_class == 'Forex':
+        # Break down the entered currency ticker
+        base_currency, target_currency = ticker[:3], ticker[3:6]
+
+        # If the last 3 letters before =X are not USD, we need to multiply the calculated shares by USD<last 3 letters>=X last closing price
+        if target_currency != 'USD':
+            forex_ticker = f"USD{target_currency}=X"
+            forex_data = yf.download(forex_ticker, period="1d")['Close'].iloc[0]
+            risk_per_trade = risk_per_trade / forex_data
+
+        # We also need to check if the first three letters of the entered currency are USD. If they are also not USD then we need to do the exact same thing we did with the last 3 letters
+        if base_currency != 'USD':
+            forex_ticker = f"USD{base_currency}=X"
+            forex_data = yf.download(forex_ticker, period="1d")['Close'].iloc[0]
+            risk_per_trade = risk_per_trade / forex_data
 
     trade_size = risk_per_trade / risk_per_unit
     return int(trade_size)
 
-
-# Function to calculate ATR-based stop-loss
-def calculate_atr_stop_level(ticker, entry_level, atr_multiplier=2):
-    data = yf.download(ticker, period='1y')
-    atr = AverageTrueRange(data['High'], data['Low'], data['Close']).average_true_range().iloc[-1]
-    stop_level = entry_level - atr * atr_multiplier
-    return stop_level
-
-# Main function
 def trade_sizing_app():
-    st.markdown("# Trade Sizing App")
-    ticker = st.text_input("Stock Ticker")
-    entry_level = st.number_input("Trade Entry Level", min_value=0.0, value=100.0, step=0.01)
-    trade_direction = st.selectbox("Trade Direction", ['Long', 'Short'])
-    account_size = st.number_input("Account Size", min_value=0.0, value=10000.0, step=100.0)
+    st.markdown("# Trade Sizing")
+
+    ticker = st.text_input("Enter the Ticker")
+    entry_level = st.number_input("Enter the Entry Level", min_value=0.0, value=100.0, step=0.01)
+    account_size = st.number_input("Enter the Account Size", min_value=0.0, value=100000.0, step=0.01)
     risk_allocation = st.number_input("Target Risk as % of Account", min_value=0.01, max_value=10.0, value=1.0, step=0.01)
-    stop_loss_override = st.number_input("Stop Loss Override (Leave blank for ATR-based stop loss)", min_value=0.0, value=0.0, step=0.01)
-    base_currency = st.text_input("Base Currency", value="USD")
-    target_currency = st.text_input("Target Currency", value="USD")
+    stop_loss_override = st.text_input("Stop Loss Override", value="")
+    asset_class = st.selectbox("Asset Class", ['Stocks', 'Options', 'Futures', 'Forex'])
 
-    if st.button("Calculate Trade Size"):
-        # Determine stop level
-        if stop_loss_override == 0.0:
-            stop_level = calculate_atr_stop_level(ticker, entry_level)
+    if ticker and st.button("Calculate Trade Size"):
+        data = yf.download(ticker, period="1d")
+        atr = ta.volatility.average_true_range(data['High'], data['Low'], data['Close'], window=14, fillna=False).iloc[-1]
+
+        if stop_loss_override:
+            stop_level = float(stop_loss_override)
         else:
-            stop_level = stop_loss_override
+            stop_level = entry_level - 2 * atr if asset_class != 'Forex' else entry_level - 0.02 * atr  # Adjust ATR multiplier for Forex
 
-        trade_size = calculate_trade_size(account_size, risk_allocation, entry_level, stop_level, base_currency, target_currency)
-        
-        dollar_risk = trade_size * abs(entry_level - stop_level)
+        trade_size = calculate_trade_size(account_size, risk_allocation, entry_level, stop_level, ticker, asset_class)
 
         st.write(f"Shares: {trade_size}")
         st.write(f"Entry: {entry_level}")
         st.write(f"Stop Level: {stop_level}")
-        st.write(f"$ Risk: {dollar_risk}")
+        st.write(f"$ Risk: {trade_size * risk_per_unit}")
 
-# Call the function
 trade_sizing_app()
 
