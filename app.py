4c1ad1b6992962e3f34e555b8f6fc0c3cd418c50
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-08-08 09:58:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 import streamlit as st
-from pages import daily_signals, indicies, positions, single_names, currency_pairs, commodities, user_input, variance_sim, trade_sizing
+from pages import indicies, single_names, currency_pairs, commodities, user_input, variance_sim, trade_sizing
 
 st.set_page_config(page_title="Multi-Page Dashboard", page_icon=":chart_with_upwards_trend:")
-
-def daily_signals_page():
-    st.sidebar.markdown("# Daily Signals")
-    daily_signals.daily_signals_app()
 
 def indicies_page():
     st.sidebar.markdown("# Indices")
     indicies.indicies_app()
-
-def positions_page():
-    st.sidebar.markdown("# Positions")
-    positions.positions_app()
 
 def single_names_page():
     st.sidebar.markdown("# Single Names")
@@ -44,9 +36,7 @@ def fwd_distributions_page():
     fwd_distributions.fwd_distributions_app()
 
 page_names_to_funcs = {
-    "Daily Signals": daily_signals_page,
     "Indices": indicies_page,
-    "Positions": positions_page,
     "Single Names": single_names_page,
     "Currency Pairs": currency_pairs_page,
     "Commodities": commodities_page,
