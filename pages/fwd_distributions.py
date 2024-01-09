ff9ccd739220b1c02f3d35b1ec8567606973b056
mslade50 <107962250+mslade50@users.noreply.github.com>
2024-01-09 13:10:24 -0500

    Update fwd_distributions.py

--- pages/fwd_distributions.py
+++ pages/fwd_distributions.py
@@ -53,7 +53,7 @@ def seasonal_return_ranks(ticker,end):
 
 	#second dataframe explicity to count the number of trading days so far this year
 	now = dt.datetime.now()+timedelta(days=1)
-	days = yf.download(ticker, start="2024-01-01", end=now)
+	days = yf.download(ticker, start="2023-12-30", end=now)
 	days["log_return"] = np.log(days["Close"] / days["Close"].shift(1))*100
 	days['day_of_year'] = days.index.day_of_year
 	days['this_yr']=days.log_return.cumsum()
@@ -688,7 +688,7 @@ def fig_creation(ticker,tgt_date_range,end_date,sigma,days,atr):
 	)
 	
 	
-	def compute_distance(row, target_values, avg_rank_weight=3.15):
+	def compute_distance(row, target_values, avg_rank_weight=0):
 	    return np.sqrt(
 	        avg_rank_weight * (row['Average_rnk'] - target_values[0])**2 +  # Increased importance
 	        (row['Trailing_1d_pct_rank'] - target_values[1])**2 +
