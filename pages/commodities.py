0b9cceda46c79823bd6a07256b82120248b1a872
mslade50 <107962250+mslade50@users.noreply.github.com>
2024-02-16 11:05:56 -0500

    Update commodities.py

--- pages/commodities.py
+++ pages/commodities.py
@@ -14,7 +14,7 @@ def seasonals_chart(tick):
 	ticker=tick
 	cycle_start=1952
 	cycle_label='Third Year of Cycle'
-	cycle_var='election'
+	cycle_var='pre_election'
 	adjust=0
 	plot_ytd="Yes"
 	all_=""
