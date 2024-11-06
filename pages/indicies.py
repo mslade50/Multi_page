931e70d699eef3e0d4d625566032392e03081735
mslade50 <107962250+mslade50@users.noreply.github.com>
2024-11-06 08:55:42 -0500

    Update indicies.py

--- pages/indicies.py
+++ pages/indicies.py
@@ -11,6 +11,15 @@ import plotly.graph_objs as go
 import json
 
 st.title("Indicies")
+def get_stock_history(ticker, period="max", end_date=None):
+	    spx1 = yf.Ticker(ticker)
+	    try:
+	        # Fetch historical data
+	        spx = spx1.history(period=period, end=end_date)
+	    except Exception as e:
+	        print("Error fetching data:", e)
+	        spx = None  # Return None or handle error as needed
+	    return spx
 def seasonals_chart(tick):
 	ticker=tick
 	cycle_start=1952
@@ -21,15 +30,7 @@ def seasonals_chart(tick):
 	all_=""
 	end_date=dt.datetime(2023,12,30)
 
-	def get_stock_history(ticker, period="max", end_date=None):
-	    spx1 = yf.Ticker(ticker)
-	    try:
-	        # Fetch historical data
-	        spx = spx1.history(period=period, end=end_date)
-	    except Exception as e:
-	        print("Error fetching data:", e)
-	        spx = None  # Return None or handle error as needed
-	    return spx
+	
 	spx = get_stock_history(ticker, end_date=end_date)
 	spx_rank=spx1.history(period="max")
 	# Calculate trailing 5-day returns
